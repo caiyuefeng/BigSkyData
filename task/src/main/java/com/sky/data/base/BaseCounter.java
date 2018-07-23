08da3b0fbbd5d14470e1d342d4f3c70c166144cc
caiyuefeng <1395945760@qq.com>
2018-07-23 15:50:06 +0800

    1、提交统计量基类接口

---
+++ task/src/main/java/com/sky/data/base/BaseCounter.java
@@ -0,0 +1,17 @@
+package com.sky.data.base;
+
+/**
+ * @Author: 蔡月峰
+ * @Description: hadoop 统计量 基类
+ * @Date : Create in 22:33 2018/3/23
+ * @Modified By:
+ */
+public interface BaseCounter {
+     String MAP_GROUP = "MAP";
+
+     String INPUT = "INPUT";
+
+     String REDUCE_GROUP = "REDUCE";
+
+     String OUTPUT = "OUTPUT";
+}
